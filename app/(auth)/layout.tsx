7a921aa7e6b5580225452b1985801dbe4f6abd59
zuma-code <zumar53@protonmail.com>
2025-11-27 15:20:09 +0000

    feat: añadir layout para páginas de autenticación

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+/**
+ * Layout para las páginas de autenticación
+ * 
+ * Este layout envuelve las páginas de login y registro
+ */
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
+
